a7831ea1984e9ecda0899ab699bf083bf9112050
Bruce Wayne <91925396+brucelwayne@users.noreply.github.com>
2023-10-31 21:37:51 +0800

    ProductList Setting

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,14 @@
 
 use Brucelwayne\Contact\Controllers\ContactUsController;
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::middleware('web')->group(function () {
-    //show contact us form
-    Route::get('/contact-us', [ContactUsController::class, 'index'])
-        ->name('contact-us');
-    //save post data
-    Route::post('/contact-us', [ContactUsController::class, 'store']);
-});
+Route::middleware(['web', 'localizationRedirect'])
+    ->prefix(LaravelLocalization::setLocale())
+    ->group(function () {
+        //show contact us form
+        Route::get('/contact-us', [ContactUsController::class, 'index'])
+            ->name('contact-us');
+        //save post data
+        Route::post('/contact-us', [ContactUsController::class, 'store']);
+    });
